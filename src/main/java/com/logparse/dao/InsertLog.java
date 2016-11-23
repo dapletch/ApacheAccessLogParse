2aa6bd88a0da2fd916ababf52125716ab43a72d4
dapletch <seth.pletcher@gmail.com>
2016-11-22 19:27:53 -0500

    Cleaned up closeConnection() method to make it more aesthetically pleasing, and cleaned up the code for writeLogToDb() method.

--- src/main/java/com/logparse/dao/InsertLog.java
+++ src/main/java/com/logparse/dao/InsertLog.java
@@ -13,6 +13,10 @@ public class InsertLog {
 
     private Logger logger = Logger.getLogger(InsertLog.class);
 
+    private PreparedStatement preparedStatement = null;
+
+    private Connection connection = null;
+
     private String insertRecord = "INSERT INTO log_data (ip_address, remote_user, time_accessed, request, stat_cd, bytes_sent, time_entered)"
             + " VALUES (?, ?, ?, ?, ?, ?, ?)";
 
@@ -28,36 +32,39 @@ public class InsertLog {
         return null;
     }
 
-    private void closeConnection(Connection connection, PreparedStatement ps) throws SQLException {
-        if (ps != null) {
-            ps.close();
-            logger.info("Prepared Statement closed successfully");
-        }
-        if (connection != null) {
+    private void closeConnection() {
+        try {
+            preparedStatement.close();
             connection.close();
-            logger.info("Database connection closed successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     public void writeLogToDb(List<LogRecord> logRecords) throws SQLException, ClassNotFoundException {
 
-        Connection connection = getConnection();
-        PreparedStatement ps;
-
-        ps = connection.prepareStatement(insertRecord);
-
-        for (LogRecord logRecord: logRecords) {
-            ps.setString(1, logRecord.getIpAddress());
-            ps.setString(2, logRecord.getRemoteUser());
-            ps.setTimestamp(3, new Timestamp(logRecord.getTimeAccessed().getMillis()));
-            ps.setString(4, logRecord.getRequest());
-            ps.setInt(5, logRecord.getStatCode());
-            ps.setInt(6, logRecord.getBytesSent());
-            ps.setTimestamp(7, new Timestamp(logRecord.getTimeEntered().getMillis()));
-            //logger.info(logRecord.toString());
-            ps.executeUpdate();
+        if (connection == null) {
+            connection = getConnection();
         }
 
-        closeConnection(connection, ps);
+        if (preparedStatement == null) {
+            preparedStatement = connection.prepareStatement(insertRecord);
+        }
+
+        preparedStatement.clearParameters();
+
+        for (LogRecord logRecord: logRecords) {
+            preparedStatement.setString(1, logRecord.getIpAddress());
+            preparedStatement.setString(2, logRecord.getRemoteUser());
+            preparedStatement.setTimestamp(3, new Timestamp(logRecord.getTimeAccessed().getMillis()));
+            preparedStatement.setString(4, logRecord.getRequest());
+            preparedStatement.setInt(5, logRecord.getStatCode());
+            preparedStatement.setInt(6, logRecord.getBytesSent());
+            preparedStatement.setTimestamp(7, new Timestamp(logRecord.getTimeEntered().getMillis()));
+            //logger.info(logRecord.toString());
+            preparedStatement.execute();
+        }
+
+        closeConnection();
     }
 }
